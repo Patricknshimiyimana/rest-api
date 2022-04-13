26b291cc97491162d55a9d9f8088632e359367f2
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-04-13 11:39:31 +0200

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes") 
 
+const dotenv = require("dotenv")
+dotenv.config();
+
 // Connect to MongoDB database
-mongoose.connect("mongodb+srv://pazzo:test123@cluster0.4qk2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
         app.use(express.json()) 
